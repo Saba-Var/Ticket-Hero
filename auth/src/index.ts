d019e7e4a268f53deee6bf34184c231e8b246331
Saba-Var <vartasashvili94@gmail.com>
2024-01-04 22:07:54 +0400

    feat: add ingress controller

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,10 @@ import express from 'express'
 const app = express()
 app.use(json())
 
+app.get('/api/users/currentuser', (req, res) => {
+  res.send('Test use data')
+})
+
 const port = 3000
 app.listen(port, () => {
   console.log(`Auth service listening at http://localhost:${port}`)
