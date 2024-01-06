28722c15a33d6401703ae963582a677767e1746c
Saba-Var <vartasashvili94@gmail.com>
2024-01-06 11:37:08 +0400

    feat: connect to mongodb instace

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,7 @@ import { signOutRouter } from './routes/sign-out'
 import { signInRouter } from './routes/sign-in'
 import { signUpRouter } from './routes/sign-up'
 import { json } from 'body-parser'
+import mongoose from 'mongoose'
 import express from 'express'
 import 'express-async-errors'
 
@@ -22,7 +23,18 @@ app.all('*', () => {
 
 app.use(errorHandler)
 
-const port = 3000
-app.listen(port, () => {
-  console.log(`Auth service listening at http://localhost:${port}`)
-})
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth')
+    console.log('Connected to MongoDB')
+  } catch (err) {
+    console.error(err)
+  }
+
+  const serverPort = 3000
+  app.listen(serverPort, () => {
+    console.log(`Auth service listening at http://localhost:${serverPort}`)
+  })
+}
+
+start()
