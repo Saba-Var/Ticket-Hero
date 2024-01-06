4add0df0b8ca64d6a5c40d2b08874e24fac57090
Saba-Var <vartasashvili94@gmail.com>
2024-01-06 13:08:07 +0400

    chore: move user related types into separate file

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,15 +1,6 @@
+import type { UserAttrs, UserDoc, UserModel } from './user.types'
+import { Password } from '../services/password'
 import mongoose from 'mongoose'
-
-interface UserAttrs {
-  email: string
-  password: string
-}
-
-interface UserModel extends mongoose.Model<unknown> {
-  build(attrs: UserAttrs): UserDoc
-}
-
-interface UserDoc extends mongoose.Document, UserAttrs {}
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -24,5 +15,13 @@ const userSchema = new mongoose.Schema({
 })
 
 userSchema.statics.build = (attrs: UserAttrs) => new User(attrs)
+
+userSchema.pre('save', async function (done) {
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'))
+    this.set('password', hashed)
+  }
+  done()
+})
 
 export const User = mongoose.model<UserDoc, UserModel>('User', userSchema)
