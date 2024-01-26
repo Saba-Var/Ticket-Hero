ecb166bd56b92db9536d23ed4b3cea003a214d96
Saba-Var <vartasashvili94@gmail.com>
2024-01-26 22:18:47 +0400

    chore: format user model properties

--- auth/src/models/user/user.ts
+++ auth/src/models/user/user.ts
@@ -2,17 +2,29 @@ import type { UserAttrs, UserDoc, UserModel } from './types'
 import { Password } from '../../services/password'
 import mongoose from 'mongoose'
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    unique: true,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-})
+  {
+    toJSON: {
+      transform(_doc, ret) {
+        ret.id = ret._id
+        delete ret._id
+        delete ret.password
+        delete ret.__v
+      },
+    },
+  }
+)
 
 userSchema.statics.build = (attrs: UserAttrs) => new User(attrs)
 
