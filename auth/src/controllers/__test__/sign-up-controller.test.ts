734cb4a9f3015bc8af0d54c5eab924ffd9567297
Saba-Var <vartasashvili94@gmail.com>
2024-01-28 00:15:50 +0400

    test: sign up controller invalid payload tests

--- auth/src/controllers/__test__/sign-up-controller.test.ts
+++ auth/src/controllers/__test__/sign-up-controller.test.ts
@@ -10,3 +10,23 @@ it('Returns a 201 status code on successful sign up', async () => {
     })
     .expect(201)
 })
+
+it('Returns a 422 status code with an invalid email', async () => {
+  return request(app)
+    .post('/api/users/sign-up')
+    .send({
+      email: 'testgmail.com',
+      password: 'password123',
+    })
+    .expect(422)
+})
+
+it('Returns a 422 status code with an invalid password', async () => {
+  return request(app)
+    .post('/api/users/sign-up')
+    .send({
+      email: 'test@gmail.com',
+      password: 'p',
+    })
+    .expect(422)
+})
