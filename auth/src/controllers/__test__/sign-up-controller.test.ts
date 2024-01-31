dc3030db6b97cd2a6a3efdd94170f6d4ad2eeb4f
Saba-Var <vartasashvili94@gmail.com>
2024-01-31 20:52:59 +0400

    test: add tests for duplicate emails and cookie

--- auth/src/controllers/__test__/sign-up-controller.test.ts
+++ auth/src/controllers/__test__/sign-up-controller.test.ts
@@ -30,3 +30,23 @@ it('Returns a 422 status code with an invalid password', async () => {
     })
     .expect(422)
 })
+
+it('Return a 401 for duplicate emails', async () => {
+  const credentials = {
+    email: 'test@gmail.com',
+    password: 'password123',
+  }
+  await request(app).post('/api/users/sign-up').send(credentials).expect(201)
+  await request(app).post('/api/users/sign-up').send(credentials).expect(400)
+})
+
+it('Sets a cookie after successful sign up', async () => {
+  const response = await request(app)
+    .post('/api/users/sign-up')
+    .send({
+      email: 'test@gmail.com',
+      password: 'password123',
+    })
+    .expect(201)
+  expect(response.get('Set-Cookie')).toBeDefined()
+})
